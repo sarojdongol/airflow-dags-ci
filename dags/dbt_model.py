69b1c08dc7ca49ea97fcb863a62d0c78190c115b
saroj <saroj.dongol@ir.com>
2022-03-16 15:14:16 +1100

    test dag with sync ci

--- dags/dbt_model.py
+++ dags/dbt_model.py
@@ -1,33 +1,48 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
 from airflow import DAG
-from airflow_dbt.operators.dbt_operator import (
-    DbtSeedOperator,
-    DbtSnapshotOperator,
-    DbtRunOperator,
-    DbtTestOperator
-)
-from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
 
 default_args = {
-  'dir': '/srv/app/dbt',
-  'start_date': days_ago(0)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
 }
 
-with DAG(dag_id='dbt', default_args=default_args, schedule_interval='@daily') as dag:
-  dbt_seed = DbtSeedOperator(
-    task_id='dbt_seed',
-  )
+dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
 
-  dbt_snapshot = DbtSnapshotOperator(
-    task_id='dbt_snapshot',
-  )
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
-  dbt_run = DbtRunOperator(
-    task_id='dbt_run',
-  )
+t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
 
-  dbt_test = DbtTestOperator(
-    task_id='dbt_test',
-    retries=0,  # Failing tests would fail the task, and we don't want Airflow to try again
-  )
+templated_command = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
 
-  dbt_seed >> dbt_snapshot >> dbt_run >> dbt_test
+t3 = BashOperator(
+    task_id="templated",
+    bash_command=templated_command,
+    params={"my_param": "Parameter I passed in"},
+    dag=dag,
+)
+
+t2.set_upstream(t1)
+t3.set_upstream(t1)
