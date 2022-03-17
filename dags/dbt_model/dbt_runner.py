23b2ecd656c00d434c0aa5d39996cedd37255a94
saroj <saroj.dongol@ir.com>
2022-03-17 22:12:38 +1100

    Fixed git issue

--- dags/dbt_model/dbt_runner.py
+++ dags/dbt_model/dbt_runner.py
@@ -18,7 +18,7 @@ default_args = {
 
 
 with DAG(
-    dag_id="DBT_runner",
+    dag_id="DBTrunner",
     description="Databricks silver Builder",
     start_date=datetime(2022, 3, 17),
     schedule_interval="@daily",
