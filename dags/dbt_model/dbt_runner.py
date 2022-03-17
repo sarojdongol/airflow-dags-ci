b9a6fa554ba7260daa820c4af5345dd8d78727f3
saroj <saroj.dongol@ir.com>
2022-03-17 22:19:14 +1100

    updated dag id

--- dags/dbt_model/dbt_runner.py
+++ dags/dbt_model/dbt_runner.py
@@ -18,7 +18,7 @@ default_args = {
 
 
 with DAG(
-    dag_id="DBTrunner",
+    dag_id="DBT-runner",
     description="Databricks silver Builder",
     start_date=datetime(2022, 3, 17),
     schedule_interval="@daily",
