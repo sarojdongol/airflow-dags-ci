e8737a021b7c735ffbb4bc5d7e2f8826a3533e96
saroj <saroj.dongol@ir.com>
2022-03-17 22:10:50 +1100

    dbt folder added

--- dags/dbt_model/dbt_runner.py
+++ dags/dbt_model/dbt_runner.py
@@ -4,13 +4,8 @@ Following code is template file to run dbt jobs using dbt operators.
 from airflow import DAG
 from airflow_dbt.operators.dbt_operator import (
     DbtRunOperator,
-    DbtTestOperator
 )
 from airflow.operators.python_operator import PythonOperator
-from git import Repo
-import os
-import logging
-import shutil
 from datetime import datetime
 
 
@@ -22,29 +17,6 @@ default_args = {
 }
 
 
-os.environ["GIT_PYTHON_REFRESH"] = "quiet"
-
-
-def IsWorkspaceAvailable(workspace_name):
-    check_result = os.path.isdir(workspace_name)
-    if check_result == True:
-        logging.info("Workspace Exists so removing it")
-        shutil.rmtree(workspace_name)
-
-
-def GitClone(git_url, user_name, access_token=None, branch_name=None):
-    ''''''
-    if access_token == None:
-        raise Exception("Token cannot be None")
-    else:
-        IsWorkspaceAvailable("dbt_workspace")
-        url_after_split = git_url.split("@")[1]
-        Repo.clone_from(
-            f"https://{user_name}:{access_token}@{url_after_split}",
-            'dbt_workspace',
-            branch=branch_name)
-
-
 with DAG(
     dag_id="DBT_runner",
     description="Databricks silver Builder",
@@ -53,12 +25,9 @@ with DAG(
     default_args=default_args
 ) as dag:
 
-    clone_dbt_repo = PythonOperator(task_id="clone_dbt_repo", python_callable=GitClone, op_kwargs={
-            "git_url": "https://sarojdongol@bitbucket.org/cartiga/cartiga_dw.git",
-            "user_name": "sarojdongol",
-            "access_token": "zgqdr93kXMuVDcJrxDTQ",
-            "branch_name": "silvertbl/accountfirm"
-
-        })
-
-clone_dbt_repo
+    dbt_run = DbtRunOperator(
+        task_id='dbt_run',
+        dbt_bin='/usr/local/airflow/.local/bin/dbt',
+        profiles_dir='/usr/local/airflow/dags/dbt_model/cartiga_dbt_model/',
+        dir='/usr/local/airflow/dags/dbt_model/cartiga_dbt_model/'
+    )
